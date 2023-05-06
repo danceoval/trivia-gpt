49e6eb7ea5e6fac82bfc963ae101d4b04682a263
danceoval <daniel.sohval@gmail.com>
2023-05-06 10:54:26 -0400

    initial commit;

---
+++ main.py
@@ -0,0 +1,48 @@
+import openai
+openai.api_key  = 'XXXXX'
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.2,
+    )
+    return response.choices[0].message["content"]
+
+theme = """
+Countries that no longer exist
+"""
+
+number = 5
+
+gen_prompt = f"""
+Generate {number} challenging trivia questions based on the theme, delimited by triple backticks.
+
+```{theme}```
+
+Questions should be of the following formats: Multiple Choice, Fill in the Blank, and Open Ended.
+
+Here are examples of each question:
+
+Multiple Choice:
+Question: In what year did the Titanic sink?
+Answers: 
+A) 1910
+B) 1912 (correct)
+C) 1890
+D) 1921
+
+Fill in the Blank:
+Question: In 2014, the Winter Olympics were hosted in _____.
+Answer: "Sochi"
+
+Open Ended:
+Question: "What was the name of the horse the Roman Emperor Caligula appointed to Senate?"
+Answer: "Incitatus"
+
+
+"""
+
+response = get_completion(gen_prompt)
+print(response)
